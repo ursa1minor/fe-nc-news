404bad43753e74b9206cc585e1c959833b77ec37
tom <tom@michaels-MacBook-Pro.local>
2022-09-27 14:49:39 +0100

    Ticket 4 Display article list

---
+++ src/components/Articles.jsx
@@ -0,0 +1,38 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+const Articles = () => {
+    const [articles, setArticles] = useState([]);
+
+useEffect(() => {
+    axios
+    .get('https://ursula-nc.herokuapp.com/api/articles')
+    .then(({ data: { articles } }) => {
+        setArticles(articles);
+    })
+    .catch(err => {
+        // 400 or 500 errors here
+    })
+}, []);
+
+console.log(articles, '<- articles')
+
+return (
+    <main>
+        <ul className='Articles'>
+        {articles.map((article) => {
+            return (
+               <li key={article.article_id}>
+               <h3>{article.title}</h3>
+               <h4>By {article.author}</h4>
+               <p> Topic: {article.topic}</p>
+               <br></br>
+               </li>
+                )
+            })}
+        </ul>
+    </main>
+);
+};
+
+export default Articles;
