949e48f95bbc915c056eee610043994faacc8240
tom <tom@michaels-MacBook-Pro.local>
2022-10-09 15:26:58 +0100

    make About page accurate re functionality

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
         <br></br>
         <p className='about'>Northcoders News is brought to you today by Ursula Ackah in collaboration with Northcoders!</p>
         <br></br>
-        <p className='about'>- Log in on the <Link to="/users">Users</Link> page</p>
+        <p className='about'>- View users on the <Link to="/users">Users</Link> page</p>
         <p className='about'>- View articles on the <Link to="/articles">Articles</Link> page</p>
         <p className='about'>- View articles by topic via the <Link to="/topics">Topics</Link> page</p>
         <p className='about'>- Vote for articles</p>
