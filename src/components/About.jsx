2167af1adfde85b281d8aae053d65ade375e6a05
tom <tom@michaels-MacBook-Pro.local>
2022-10-12 23:49:14 +0100

    Modify About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,11 +8,12 @@ const About = () => {
         <br></br>
         <strong><p className='about'>Northcoders News is brought to you today by Ursula Ackah in collaboration with Northcoders!</p></strong>
         <br></br>
-        <p className='about'>- <Link to="/users">Log in</Link> as one of six users</p>
+       
         <p className='about'>- View articles on the <Link to="/articles">Articles</Link> page</p>
         <p className='about'>- View articles by topic via the <Link to="/topics">Topics</Link> page</p>
         <p className='about'>- Vote for articles</p>
-        <p className='about'>- Add and delete comments</p>
+        <p className='about'>- <Link to="/users">Log in</Link> as one of six users</p>
+        <p className='about'>- Add and delete comments when logged in</p>
        
         </section>
     );
